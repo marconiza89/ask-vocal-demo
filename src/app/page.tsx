bda6afa6727f9e32365fee27b4385a243faec3d5
Allstudiosweb <marconiza@live.it>
2025-10-01 12:40:44 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <div className='flex flex-col items-center justify-center' style={{ maxWidth: 760, margin: '40px auto', padding: 16 }}>
             <Image
-                src="/ask.png"
+                src="/ASK.png"
                 alt="Logo"
                 width={160}
                 height={160}
